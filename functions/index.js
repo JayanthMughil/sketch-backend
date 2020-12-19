746558d25c7fdac4d79dc8336a3cad673d2a0a59
abaian-logward <jayanthanan.abaian@logward.com>
2020-12-19 22:23:38 +0530

    create room

--- functions/index.js
+++ functions/index.js
@@ -5,11 +5,31 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-// Take the text parameter passed to this HTTP endpoint and insert it into 
-// Cloud Firestore under the path /messages/:documentId/original
-exports.isWorking = functions.https.onRequest(async (req, res) => {
-  // Push the new message into Cloud Firestore using the Firebase Admin SDK.
-  const writeResult = await admin.firestore().collection('rooms').add({msg: 'hi'});
-  // Send back a message that we've successfully written the message
-  res.json({result: `Message with ID: ${writeResult.id} added.`});
+const generateRandomString = (length) => {
+  let result           = '';
+  let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let charactersLength = characters.length;
+  for ( let i = 0; i < length; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+exports.createRoom = functions.https.onCall(async (data, context) => {
+
+  let documentList = [];
+  let roomcode = "";
+  const documents = await admin.firestore().collection('rooms').get();
+
+  for (document in documents.docs) {
+    documentList.push(document.id)
+  }
+
+  while (roomcode === "" || documentList.includes(roomcode)) {
+    roomcode = generateRandomString(6);
+  }
+
+  const writeDoc = await admin.firestore().collection('rooms').doc(roomcode).set({'participants': [data.name], 'messages': []});
+
+  return roomcode;
 });
