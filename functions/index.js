5ba46a8bc5022fa53596a564203063eece21239b
abaian-logward <jayanthanan.abaian@logward.com>
2020-12-19 21:18:14 +0530

    random commit

--- functions/index.js
+++ functions/index.js
@@ -8,5 +8,8 @@ admin.initializeApp();
 // Take the text parameter passed to this HTTP endpoint and insert it into 
 // Cloud Firestore under the path /messages/:documentId/original
 exports.isWorking = functions.https.onRequest(async (req, res) => {
-  console.log("helloo");
+  // Push the new message into Cloud Firestore using the Firebase Admin SDK.
+  const writeResult = await admin.firestore().collection('rooms').add({msg: 'hi'});
+  // Send back a message that we've successfully written the message
+  res.json({result: `Message with ID: ${writeResult.id} added.`});
 });
